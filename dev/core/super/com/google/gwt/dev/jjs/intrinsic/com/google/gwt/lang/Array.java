de852fade48b7f2bdb56c951302a687cbc1d6052
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2015-08-14 11:15:25 +0200

    Fix guava incompatibility caused by https://gwt-review.googlesource.com/#/c/13241/

--- dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/Array.java
+++ dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/Array.java
@@ -19,6 +19,7 @@ import static javaemul.internal.InternalPreconditions.checkArrayType;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+import javaemul.internal.ArrayHelper;
 import javaemul.internal.annotations.DoNotInline;
 import javaemul.internal.annotations.HasNoSideEffects;
 
@@ -88,6 +89,14 @@ public final class Array {
           elementTypeId, elementTypeCategory, result);
     }
     return result;
+  }
+
+  /**
+   * Creates an empty array of the exact same type as a given array, with the
+   * specified length.
+   */
+  public static <T> T[] createFrom(T[] array, int length) {
+    return ArrayHelper.createFrom(array, length);
   }
 
   /**
